e1cc352f74ef8cdada01a97e13e965e026325752
Weslei Franca <weslwifranca123@gmail.com>
2022-11-04 10:10:08 -0300

    first commit

---
+++ assets/js/mapa-mobile.js
@@ -0,0 +1,29 @@
+
+const idA1 = document.getElementById('a1')
+const idA2 = document.getElementById('a2')
+const idA3= document.getElementById('a3')
+const idA4 = document.getElementById('a4')
+const idA5 = document.getElementById('a5')
+const idA6 = document.getElementById('a6')
+const idA7 = document.getElementById('a7')
+const idA8 = document.getElementById('a8')
+const idA9 = document.getElementById('a9')
+const idA10 = document.getElementById('a10')
+const idA11 = document.getElementById('a11')
+const idA12 = document.getElementById('a12')
+const idA13 = document.getElementById('a13')
+const idA14 = document.getElementById('a14')
+const idA15 = document.getElementById('a15')
+const idA16 = document.getElementById('a16')
+const idA17 = document.getElementById('a17')
+const idA18 = document.getElementById('a18')
+const idA19 = document.getElementById('a19')
+const idA20 = document.getElementById('a20')
+const idA21 = document.getElementById('a21')
+
+
+
+// const clickMapa = () => {
+
+
+// setTimeout(() => { ids }, 4000)
