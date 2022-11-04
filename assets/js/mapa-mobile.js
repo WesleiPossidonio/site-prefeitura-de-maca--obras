f77a36196d5fc644715645b891c2b2020495ecb9
Weslei Franca <weslwifranca123@gmail.com>
2022-11-04 13:30:04 -0300

    updat mapa-mobile.js

--- assets/js/mapa-mobile.js
+++ assets/js/mapa-mobile.js
@@ -1,7 +1,6 @@
-
 const idA1 = document.getElementById('link1')
 const idA2 = document.getElementById('link2')
-const idA3= document.getElementById('link3')
+const idA3 = document.getElementById('link3')
 const idA4 = document.getElementById('link4')
 const idA5 = document.getElementById('link5')
 const idA6 = document.getElementById('link6')
@@ -20,3 +19,91 @@ const idA18 = document.getElementById('link18')
 const idA19 = document.getElementById('link19')
 const idA20 = document.getElementById('link20')
 const idA21 = document.getElementById('link21')
+
+
+idA1.addEventListener('touchstart', () => {
+    idA1.removeAttribute("href");
+ }, false);
+
+ idA2.addEventListener('touchstart', () => {
+    idA2.removeAttribute("href");
+ }, false);
+
+ idA3.addEventListener('touchstart', () => {
+    idA3.removeAttribute("href");
+ }, false); 
+
+ idA4.addEventListener('touchstart', () => {
+    idA4.removeAttribute("href");
+ }, false); 
+
+ idA5.addEventListener('touchstart', () => {
+    idA5.removeAttribute("href");
+ }, false); 
+
+ idA6.addEventListener('touchstart', () => {
+    idA6.removeAttribute("href");
+ }, false);  
+
+ idA7.addEventListener('touchstart', () => {
+    idA7.removeAttribute("href");
+ }, false);
+
+ idA8.addEventListener('touchstart', () => {
+    idA8.removeAttribute("href");
+ }, false);
+  
+ idA9.addEventListener('touchstart', () => {
+    idA9.removeAttribute("href");
+ }, false);
+  
+ idA10.addEventListener('touchstart', () => {
+    idA10.removeAttribute("href");
+ }, false);
+  
+
+ idA11.addEventListener('touchstart', () => {
+    idA11.removeAttribute("href");
+ }, false); 
+
+ idA12.addEventListener('touchstart', () => {
+    idA12.removeAttribute("href");
+ }, false); 
+
+ idA13.addEventListener('touchstart', () => {
+    idA13.removeAttribute("href");
+ }, false); 
+
+ idA14.addEventListener('touchstart', () => {
+    idA14.removeAttribute("href");
+ }, false); 
+
+ idA15.addEventListener('touchstart', () => {
+    idA15.removeAttribute("href");
+ }, false); 
+
+ idA16.addEventListener('touchstart', () => {
+    idA16.removeAttribute("href");
+ }, false); 
+
+ idA17.addEventListener('touchstart', () => {
+    idA17.removeAttribute("href");
+ }, false); 
+
+ idA18.addEventListener('touchstart', () => {
+    idA18.removeAttribute("href");
+ }, false); 
+
+ idA19.addEventListener('touchstart', () => {
+    idA19.removeAttribute("href");
+ }, false); 
+
+
+ idA20.addEventListener('touchstart', () => {
+    idA20.removeAttribute("href");
+ }, false); 
+
+ idA21.addEventListener('touchstart', () => {
+    idA21.removeAttribute("href");
+ }, false); 
+  
